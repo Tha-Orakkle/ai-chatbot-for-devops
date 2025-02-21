1a4da38f57ff57191e0bb2950ec3bc96bba461aa
Adegbiran-Ayinoluwa Paul <106776486+Tha-Orakkle@users.noreply.github.com>
2025-02-21 11:13:19 +0000

    feat: process user input to trigger a post request to telex webhook
    new thread is created to handle request. The request is classified to determine if a github logs should be fetched
    
    or an AI response should be generated and sent to channel webhook

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,49 @@
 from flask import Blueprint, jsonify, request
-import aiohttp, asyncio
+import threading
 
 from .integration_config import integration_config
-from .helpers import request_handler
+from .helpers import call_request_handler_in_thread
+
 
 CHANNEL_WEBHOOK_BASE_URL = "https://ping.telex.im/v1/webhooks"
 
 bp = Blueprint('routes', __name__)
+
 
 # ROUTES
 
 # handles request to the integration specification
 @bp.route('/integration.json', methods=['GET'])
 def get_integration_config():
+    """
+    Returns json specification for Telex integration
+    """
     return jsonify(integration_config)
 
 
-# handles the request from telex
+# handles the request from telex.
 @bp.route('/webhook', methods=['POST'])
 async def target():
+    """
+    creates a new thread and handles all tasks in the new thread.
+    retrives payload from request and passes it to the task that will
+    be executed asynchronously in new thread
+    """
     json_data = request.get_json()
-    channel_url =  f"{CHANNEL_WEBHOOK_BASE_URL}/{json_data.get("channel_id")}"
+    channel_url = f"{CHANNEL_WEBHOOK_BASE_URL}/{json_data.get('channel_id')}"
 
     text = json_data.get('message').strip()
-    if text[:7] != '/devops': return '', 204
-
+    if not text.startswith('/devops'): # only respond to task prefixed with /devops
+        return '', 204
+    
     text = text[7:].strip()
     settings = {x['label']: x['default'] for x in json_data['settings']}
 
-    asyncio.create_task(request_handler(channel_url, text, settings))
-
+    #start new thread
+    thread = threading.Thread(
+        target=call_request_handler_in_thread,
+        args=(channel_url, text, settings))
+    thread.start()
+    
     return jsonify({
-        "status": "request processing"
-    })
+        "status": "request processing"})
