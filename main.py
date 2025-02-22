bb52bddce37661611c73ef8cac17cb854fe06e7b
Adegbiran-Ayinoluwa Paul <106776486+Tha-Orakkle@users.noreply.github.com>
2025-02-22 11:07:17 +0000

    feat: Add endpoint for error 404 handler

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from flask import jsonify
+
 from app import app
 from app.routes import bp
 
 
 app.register_blueprint(bp)
+
+# handles 404 errors 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'message': 'Not Found',
+        'status': 404,
+        "specification_url": "https://devbot-integration-spec.up.railway.app/api/integration.json",
+        "target_url": "https://54.175.31.188/v1/webhook"
+    }), 404
