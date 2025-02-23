276f270502bad5466032ba708d5345c0c4ca24cb
Adegbiran-Ayinoluwa Paul <adegbiranayinoluwa.paul@yahoo.com>
2025-02-23 10:51:18 +0000

    update: get channel_url from environmental variable

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 import threading
+import os
 
 from .integration_config import SPECIFICATION
 from .helpers import call_request_handler_in_thread
@@ -40,7 +41,7 @@ async def target():
     be executed asynchronously in new thread
     """
     json_data = request.get_json()
-    channel_url = f"{CHANNEL_WEBHOOK_BASE_URL}/{json_data.get('channel_id')}"
+    channel_url = os.getenv("CHANNEL_URL")
 
     text = json_data.get('message').strip()
     if not text.startswith('/devbot'): # only respond to task prefixed with /devops
